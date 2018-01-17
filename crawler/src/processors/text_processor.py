c4b750e281f7d2b4001b23020d6fbbe48aee5bf5
Kirill Kupriyanov <kupriyanovkirill@gmail.com>
2018-01-17 05:24:12 +0300

    Processors for date_in, date_out. Formatting.
    Text processors for date_in and date_out, constants are now upper-cased.

--- crawler/src/processors/text_processor.py
+++ crawler/src/processors/text_processor.py
@@ -1,6 +1,9 @@
-#coding=utf-8
+# coding=utf-8
 
 import re
+from time import gmtime, strftime
+
+
 def remove_junk(s):
     s = s.strip()
     # Dixy's wrong non-breaking space.
@@ -12,6 +15,10 @@ def remove_junk(s):
     return s
 
 
+def remove_literals_ws(s):
+    return re.sub('[а-яА-Я\s]', '', s)
+
+
 def try_float(val):
     res = []
     try:
@@ -19,7 +26,7 @@ def try_float(val):
     except ValueError:
         return val
     return res
- 
+
 
 def concat(left, right, sep):
     return try_float(remove_junk(left) + sep + remove_junk(right))
@@ -27,6 +34,28 @@ def concat(left, right, sep):
 
 def process(val):
     return try_float(remove_junk(val))
+
+
+def split_by(val, delim):
+    return val.split(delim)
+
+
+def make_date(raw):
+    year = strftime('%Y', gmtime())
+    month = raw.split('/')[1]
+    day = raw.split('/')[0]
+
+    return '{Y}-{m}-{d}'.format(Y=year, m=month, d=day)
+
+
+def parse_date_in(s):
+    raw = split_by(remove_literals_ws(remove_junk(s)), '—')[0]
+    return make_date(raw)
+
+
+def parse_date_out(s):
+    raw = split_by(remove_literals_ws(remove_junk(s)), '—')[1]
+    return make_date(raw)
 
 
 # EOF
