86b706651ccab1be2f0c5d9094ac3a651dc3c678
Kirill Kupriyanov <kupriyanovkirill@gmail.com>
2018-03-17 20:08:41 +0300

    Set splash settings.

--- easysales/easysales/settings.py
+++ easysales/easysales/settings.py
@@ -13,12 +13,20 @@ DOWNLOADER_MIDDLEWARES = {
     # Downloader side
 }
 
-SPIDER_MIDDLEWARES = {
-    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
-}
-SPLASH_URL = 'http://127.0.0.1:8050/'
-DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
-HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
+# DOWNLOADER_MIDDLEWARES = {
+#     # Engine side
+#     'scrapy_splash.SplashCookiesMiddleware': 723,
+#     'scrapy_splash.SplashMiddleware': 725,
+#     'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+#     # Downloader side
+# }
+
+# SPIDER_MIDDLEWARES = {
+#     'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+# }
+# SPLASH_URL = 'http://127.0.0.1:8050/'
+# DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+# HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
 
 
 # EOF
