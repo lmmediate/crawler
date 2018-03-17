8c608db24a96e0a27c09f0402971393877c8a4f3
Kirill Kupriyanov <kupriyanovkirill@gmail.com>
2018-03-17 17:14:39 +0300

    Add splash to `settings.py`. PipReqs new run.
    (Regenerate `requirements.txt` to use splash.)

--- easysales/easysales/settings.py
+++ easysales/easysales/settings.py
@@ -5,6 +5,21 @@ BOT_NAME = 'easysales'
 SPIDER_MODULES = ['easysales.spiders']
 NEWSPIDER_MODULE = 'easysales.spiders'
 
+DOWNLOADER_MIDDLEWARES = {
+    # Engine side
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+    # Downloader side
+}
+
+SPIDER_MIDDLEWARES = {
+    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+}
+SPLASH_URL = 'http://127.0.0.1:8050/'
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
+
 
 # EOF
 
