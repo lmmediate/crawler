bda79da022f40d38bb60354d34351c40d4986e12
Kirill Kupriyanov <kupriyanovkirill@gmail.com>
2018-04-09 17:21:14 +0300

    Edit port.

--- easysales/config.py
+++ easysales/config.py
@@ -1,7 +1,7 @@
 const =\
 {
-    'SHOPS_INFO_API': 'http://46.17.44.125:8080/api/shops',
-    'ADD_ITEM_API': 'http://localhost:8080/api/shops',
+    'SHOPS_INFO_API': 'http://46.17.44.125/api/shops',
+    'ADD_ITEM_API': 'http://localhost/api/shops',
     'URI_JSON_OUT': 'out/items.json',
     'REQUEST_HEADERS': {'Content-Type': 'application/json'},
 }
